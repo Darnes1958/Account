f3327dc22ca51f99263286b7d0b435deb6b8d62a
Darnes1958 <abdel_ati_m@yahoo.com>
2024-10-16 08:59:42 +0200

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Table::$defaultNumberLocale = 'nl';
+        FilamentColor::register([
+            'Fuchsia' =>  Color::Fuchsia,
+            'green' =>  Color::Green,
+            'blue' =>  Color::Blue,
+            'gray' =>  Color::Gray,
+        ]);
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en',]); // also accepts a closure
+        });
+        Model::unguard();
+    }
+}
