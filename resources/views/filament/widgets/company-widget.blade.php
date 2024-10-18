5a42b166ae4c1f6c04fa03390fa2d9cf7d635af7
Darnes1958 <abdel_ati_m@yahoo.com>
2024-10-18 21:59:05 +0200

    Go live

--- resources/views/filament/widgets/company-widget.blade.php
+++ resources/views/filament/widgets/company-widget.blade.php
@@ -1,6 +1,6 @@
 <x-filament-widgets::widget>
     <div class="flex w-full">
-
+        @if($has_image)
         <div class="mr-48 ">
             <x-filament::avatar
                  src="{{ asset('storage/'.$image) }}" alt="description of myimage"
@@ -8,6 +8,7 @@
                  :circular="false"
             />
         </div>
+        @endif
 
 
     </div>
